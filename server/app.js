947b5a106f1ca79b8b840e052fce9c3ebeb0b4ae
Aleksi <aleksi.hayrynen@aalto.fi>
2026-02-06 21:27:13 +0200

    NEw updates

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import postgres from "postgres";
 
 const app = new Hono();
 const counts = new Map();
+const sql = postgres();
 
 
 app.post("/", async (c) => {
@@ -45,6 +46,39 @@ app.delete("/api/count/:resource", (c) => {
   counts.set(resource, count);
 
   return c.json({ count });
+});
+
+app.get("/api/books/:bookId", async (c) => {
+  const id = Number(c.req.param("bookId"));
+  if (!Number.isInteger(id)) {
+    return c.json({ error: "Invalid book id" }, 400);
+  }
+
+  const result = await sql`SELECT *
+    FROM books
+    WHERE id = ${id}`;
+  if (result.length === 0) {
+    return c.json({ error: "Book not found" }, 404);
+  }
+  return c.json(result[0]);
+});
+
+app.post("/api/books", async (c) => {
+  const book = await c.req.json();
+
+  if (!book.title ||
+    !book.description ||
+    !book.published_at ||
+    !book.page_count) {
+    return c.json({ error: "Missing required fields" }, 400);
+  }
+
+  const result = await sql`INSERT INTO books
+    (title, description, published_at, page_count)
+    VALUES (${book.title}, ${book.description}, ${book.published_at}, ${book.page_count})
+    RETURNING *;`;
+
+  return c.json(result[0], 201);
 });
 
 export default app;
