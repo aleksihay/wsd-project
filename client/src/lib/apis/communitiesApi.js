2c6690bd98c4aaef5dc2af185f2a073c742d8053
aleksi <aleksi.hayrynen@aalto.fi>
2026-03-03 10:39:40 +0200

    API for communities

---
+++ client/src/lib/apis/communitiesApi.js
@@ -0,0 +1,26 @@
+import { PUBLIC_API_URL } from "$env/static/public";
+
+const createCommunity = async (community) => {
+    const response = await fetch(`${PUBLIC_API_URL}/apicommunities`, {
+        headers: {"Content-Type": "application/json",},
+        method: "POST",
+        body: JSON.stringify(community),
+    });
+    return await response.json();
+};
+const readCommunities = async () => {
+    const response = await fetch(`${PUBLIC_API_URL}/api/communities`);
+    return await response.json();
+};
+const readCommunity = async (id) => {
+    const response = await fetch(`${PUBLIC_API_URL}/api/communities/${id}`);
+    return await response.json();
+};
+const deleteCommunity = async (id) => {
+    const response = await fetch(`${PUBLIC_API_URL}/api/communities/${id}`, {
+        method: "DELETE",
+    });
+    return await response.json();
+};
+
+export { createCommunity, readCommunities, readCommunity, deleteCommunity };
