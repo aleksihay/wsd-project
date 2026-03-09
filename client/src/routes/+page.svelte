00b32e7d92b562515cb647e09da0a88ee98067f6
Aleksi <aleksi.hayrynen@aalto.fi>
2026-03-09 19:33:54 +0200

    Client side funtionality for registration and logging in

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,3 @@
 <h1>Welcome</h1>
 
-<ul>
-    <li><a href="/communities">Browse our communities</a></li>
-    <li><a href="/todos">Click here for todos</a></li>
-    <li><a href="/temperature">Temperature</a></li>
-</ul>
+<p>Welcome to the home page!</p>
